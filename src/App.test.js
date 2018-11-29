a26c40dadc1e622e44fc59b9f9210a2f483faa73
Michael Landry <mlandryviolin@gmail.com>
2018-11-28 22:56:33 -0500

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Room from './Room'
+import Dropdown from './Dropdown'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
+
+it('dropdown fields for Adult and children are disabled for room 2, 3, 4', () => {
+  const testRoom = (wrapper, number) => {
+    const room = wrapper.find('Room').at(1)
+    const dropdown = room.find('Dropdown').at(0)
+    const select = dropdown.find('select')
+    expect(select.prop('disabled')).toBe(true)
+  }
+
+  const wrapper = mount(<App />);
+  expect(wrapper.find('Room')).toHaveLength(4);
+  testRoom(wrapper, 2)
+  testRoom(wrapper, 3)
+  testRoom(wrapper, 4)
+});
+
+it('default values are correct', () => {
+  const wrapper = mount(<App />);
+  const room = wrapper.find('Room').at(0)
+  const dropdown = room.find('Dropdown').at(0)
+  const select = dropdown.find('select')
+  expect(select.prop('value')).toBe(1)
+
+  const children = room.find('Dropdown').at(1)
+  const select2 = children.find('select')
+  expect(select2.prop('value')).toBe(0)
+
+
+});
+
